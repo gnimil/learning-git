89d3fd7597bd2aeecbf166a5094665e2f999b8c5
Ming Li <mxli@live.com>
2014-06-19 09:29:55 -0500

    first commit

---
+++ RX.Starter/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reactive;
+using System.Reactive.Subjects;
+using System.Threading;
+
+namespace RX.Starter
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            AsyncSubjectExample();
+        }
+
+        static void WriteSequenceToConsole(IObservable<string> sequence)
+        {
+            sequence.Subscribe(Console.WriteLine);
+        }
+
+        static void BehaviorSubjectExample()
+        {
+
+            var subject = new BehaviorSubject<string>("a");
+            subject.Subscribe(Console.WriteLine);
+        }
+
+        static void AsyncSubjectExample()
+        {
+            var subject = new AsyncSubject<string>();
+            subject.OnNext("a");
+            WriteSequenceToConsole(subject);
+            subject.OnNext("b");
+            subject.OnNext("c");
+            subject.OnCompleted();
+            Console.ReadKey();
+        }
+
+    }
+}
